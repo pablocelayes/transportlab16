61a160a27177126c1f5c05a9b93704a327d65743
Pablo G Celayes <pablocelayes@gmail.com>
2016-06-12 18:26:42 +0200

    improved latlon conversion, added left direction

--- a13latlon.py
+++ a13latlon.py
@@ -2,12 +2,15 @@ from rd_coords_convert import *
 import pandas as pd
 
 conv = RDWGSConverter()
+
 df = pd.read_csv('a13/Rechts_RDCoordinaten_2011.csv', header=None)
-
-for i, row in df.iterrows():
-    new_rows.append(conv.fromRdToWgs(row))
-
+new_rows = [conv.fromRdToWgs(row) for i, row in df.iterrows()]
 df_new = pd.DataFrame(new_rows)
 df_new.columns=['lat','lon']
+df_new.to_csv('a13/Rechts_LatLonCoordinaten_2011.csv', index=False)
 
-df_new.to_csv('a13/Rechts_LatLonCoordinaten_2011.csv')
+df = pd.read_csv('a13/Links_RDCoordinaten_2011.csv', header=None)
+new_rows = [conv.fromRdToWgs(row) for i, row in df.iterrows()]
+df_new = pd.DataFrame(new_rows)
+df_new.columns=['lat','lon']
+df_new.to_csv('a13/Links_LatLonCoordinaten_2011.csv', index=False)
